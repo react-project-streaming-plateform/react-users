da7855b9ed3e96771f85770dc80ecb0ec82b84c3
jilink <jimmyfac@gmail.com>
2022-12-07 16:40:04 +0100

    groupe 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios"
+import {useEffect, useState} from 'react'
 
 function App() {
+
+  const [list,setList] = useState([])
+
+  const [body, setBody] = useState({})
+
+  async function getData() {
+    setTimeout(async () => {
+    const response = await axios.get("http://localhost:3001/users")
+    console.log("reponse", response)
+    setList(response.data)
+    }, 5000);
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+  function handleChange(e) {
+    setBody((b) => ({ ...b, [e.target.name]: e.target.value }));
+    console.log("body", body)
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+    await axios.post("http://localhost:3001/users", body)
+    getData()
+    console.log("test")
+
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      les users
+      {list.map(user => {
+        return <p key={user.id}>Je m'apelle {user.name} {user.lastname}</p>
+      })}
+      <form onSubmit={handleSubmit}>
+        <input name="name" onChange={handleChange} placeholder="name" type="text"/>
+        <input name="lastname" onChange={handleChange} placeholder="lastname" type="text"/>
+        <button type="submit">Ajouter un user</button>
+        </form>
     </div>
   );
 }
